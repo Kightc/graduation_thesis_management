70521e5e411f66418e8ddbf9a84270f9bf284db9
Kight <2578842650@qq.com>
2023-06-21 16:55:44 +0800

    6.21 pra01

--- src/main/java/usts/cs2020/BootApplication.java
+++ src/main/java/usts/cs2020/BootApplication.java
@@ -1,9 +1,19 @@
-package usts.cs2020;/**
-* Description:
-* 
-*@Author Kight
-*@Date 2023/6/21 15:59
-*@Version 1.0
-*/
+package usts.cs2020;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Description:
+ *
+ * @Author Kight
+ * @Date 2023/6/21 15:59
+ * @Version 1.0
+ */
+@SpringBootApplication
+
 public class BootApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BootApplication.class, args);
+    }
 }
--- src/main/java/usts/cs2020/model/BaseEntity.java
+++ src/main/java/usts/cs2020/model/BaseEntity.java
@@ -1,9 +1,25 @@
-package usts.cs2020.model;/**
-* Description:
-* 
-*@Author Kight
-*@Date 2023/6/21 16:20
-*@Version 1.0
-*/
-public class BaseEntity {
+package usts.cs2020.model;
+
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Description:
+ *
+ * @Author Kight
+ * @Date 2023/6/21 16:20
+ * @Version 1.0
+ */
+@Data
+public class BaseEntity implements Serializable {
+    private Long id;
+    private Date createTime;
+    private Date updateTime;
+    private Integer isDeleted;
+    private Map<String, Object> param = new HashMap<>();
+
 }
