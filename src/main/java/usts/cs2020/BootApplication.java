5b492ac38411667a95df62581e59ca2ec6e6432e
Kight <2578842650@qq.com>
2023-06-21 16:54:08 +0800

    Initial commit

---
+++ src/main/java/usts/cs2020/BootApplication.java
@@ -0,0 +1,9 @@
+package usts.cs2020;/**
+* Description:
+* 
+*@Author Kight
+*@Date 2023/6/21 15:59
+*@Version 1.0
+*/
+public class BootApplication {
+}
---
+++ src/main/java/usts/cs2020/model/BaseEntity.java
@@ -0,0 +1,9 @@
+package usts.cs2020.model;/**
+* Description:
+* 
+*@Author Kight
+*@Date 2023/6/21 16:20
+*@Version 1.0
+*/
+public class BaseEntity {
+}
